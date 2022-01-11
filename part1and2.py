874ee6927e17e09abddfeab51f0864a5ef58bdc3
Becca <beck.mae.harrow@gmail.com>
2022-01-11 12:05:48 +0000

    part 2 complete, result 2

--- part1and2.py
+++ part1and2.py
@@ -16,17 +16,32 @@ for month in range(12):
     precipitation_per_month.append(monthly_total)
 
 seattle_data = {
-    "station": station_code,
+    "station": station_code, 
     "state": "WA",
     "totalMonthlyPrecipitation": precipitation_per_month
 }
 
-with open('result1', 'w') as saving_part1: 
+with open('result1.json', 'w') as saving_part1: 
     json.dump(seattle_data, saving_part1)
 # answers to part 1 end here 
 
 #answers to part 2 begin here 
-total_yearly_rainfall = 0 
+total_yearly_precipitation = 0 
 for monthly_precipitation in precipitation_per_month: 
-    total_yearly_rainfall = total_yearly_rainfall + monthly_precipitation
-print(total_yearly_rainfall)
+    total_yearly_precipitation = total_yearly_precipitation + monthly_precipitation
+
+relative_monthly_rainfall = []
+for month in range(12): 
+    relative_current_month = precipitation_per_month[month]/total_yearly_precipitation
+    relative_monthly_rainfall.append(relative_current_month)
+print(relative_monthly_rainfall)
+
+seattle_data['realtiveMonthlyPrecipitation'] = relative_monthly_rainfall
+seattle_data['totalYearlyPrecipitation'] = total_yearly_precipitation
+
+with open('result2.json', 'w') as saving_part2: 
+    json.dump(seattle_data, saving_part2)
+# answers to part 2 end here 
+
+
+
